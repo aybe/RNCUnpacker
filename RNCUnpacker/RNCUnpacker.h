f3ed91901734f32680b80390b13783fb6c8cec54
Aybe <aybe@users.noreply.github.com>
2018-01-30 21:49:12 +0100

    Fixed header

--- RNCUnpacker/RNCUnpacker.h
+++ RNCUnpacker/RNCUnpacker.h
@@ -1,9 +1,5 @@
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the RNCUNPACKER_EXPORTS
-// symbol defined on the command line. This symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
-// RNCUNPACKER_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
+#pragma once
+
 #ifdef RNCUNPACKER_EXPORTS
 #define RNCUNPACKER_API __declspec(dllexport)
 #else
@@ -12,7 +8,7 @@
 
 #include "rnc.h"
 
-EXTERN_C RNCUNPACKER_API rnc_status unpack(const uint8_t* input, uint8_t* output)
+EXTERN_C inline RNCUNPACKER_API rnc_status unpack(const uint8_t* input, uint8_t* output)
 {
 	return rnc_unpack(input, output);
 }
